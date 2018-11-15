13eb09192fc46559457cc7eef7beb9bbf6473c4d
AVE-Desktop <virgil-andreies>
2018-11-15 09:16:58 +0200

    First Commit

---
+++ assets/js/profile.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+	 
+	$nav = $("#navigation"),
+	$slideLine = $("#slide-line"),
+	$currentItem = $("#navigation li.active");
+	
+
+	$(window).on('resize load', function() {
+		
+		if ($currentItem[0]) {
+			$slideLine.css({
+			  "width": $currentItem.width(),
+			  "left": $currentItem.position().left
+			});
+		}
+	});
+	
+	// Underline transition
+	$nav.find("li").hover(
+		// Hover on
+		function(){
+		  $slideLine.css({
+		    "width": $(this).width(),
+		    "left": $(this).position().left
+		  });
+		},
+		// Hover out
+		function(){
+		  if ($currentItem[0]) {
+		    // Go back to current
+		    $slideLine.css({
+		      "width": $currentItem.width(),
+		      "left": $currentItem.position().left
+		    });
+		  } else {
+		    // Disapear
+		    $slideLine.width(0);
+		  }
+		}
+	);
+    
+
+});
