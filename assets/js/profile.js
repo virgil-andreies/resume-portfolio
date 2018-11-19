499f221022f497645a11cf2d8c82a9584347ab2c
Virgil Andreies <Virgil.Andreies@msg.group>
2018-11-19 15:49:54 +0200

    Added sticky navigation bar functionality
    implemented sticky bar functionality along with scroll-to functionality

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,62 +1,95 @@
-$(document).ready(function() {
-	 
-	$nav = $("#navigation"),
-	$slideLine = $("#slide-line"),
-	$currentItem = $("#navigation li.active");
-	
+$(document).ready(function () {
 
-	$(window).on('resize load', function() {
-		
+	/* ======= Scrollspy ======= */
+	$('body').scrollspy({ target: '#navigation', offset: 100 });
+
+	/* ======= ScrollTo ======= */
+	$('.scrollto').on('click', function (e) {
+
+		//store hash
+		var target = this.hash;
+
+		e.preventDefault();
+
+		$('body').scrollTo(target, 800, { offset: -60, 'axis': 'y' });
+
+	});
+
+	/* *************** Fixed page nav when scrolled *************** */
+	$(window).on('scroll resize load', function () {
+
+		$('#navigation').removeClass('fixed');
+
+		var scrollTop = $(this).scrollTop();
+		var topDistance = $('#navigation').offset().top;
+
+		if ((topDistance) > scrollTop) {
+			$('#navigation').removeClass('fixed');
+			$('body').removeClass('sticky-page-nav');
+		}
+		else {
+			$('#navigation').addClass('fixed');
+			$('body').addClass('sticky-page-nav');
+		}
+
+	});
+
+	$nav = $("#navigation"),
+		$slideLine = $("#slide-line"),
+		$currentItem = $("#navigation li.active");
+
+
+	$(window).on('resize load', function () {
+
 		if ($currentItem[0]) {
 			$slideLine.css({
-			  "width": $currentItem.width(),
-			  "left": $currentItem.position().left
+				"width": $currentItem.width(),
+				"left": $currentItem.position().left
 			});
 		}
 	});
-	
+
 	// Underline transition
 	$nav.find("li").hover(
 		// Hover on
-		function(){
-		  $slideLine.css({
-		    "width": $(this).width(),
-		    "left": $(this).position().left
-		  });
+		function () {
+			$slideLine.css({
+				"width": $(this).width(),
+				"left": $(this).position().left
+			});
 		},
 		// Hover out
-		function(){
-		  if ($currentItem[0]) {
-		    // Go back to current
-		    $slideLine.css({
-		      "width": $currentItem.width(),
-		      "left": $currentItem.position().left
-		    });
-		  } else {
-		    // Disapear
-		    $slideLine.width(0);
-		  }
+		function () {
+			if ($currentItem[0]) {
+				// Go back to current
+				$slideLine.css({
+					"width": $currentItem.width(),
+					"left": $currentItem.position().left
+				});
+			} else {
+				// Disapear
+				$slideLine.width(0);
+			}
 		}
 	);
 
 	/* *************** Chart ************* */
-    
-	$('.chart').easyPieChart({		
-		barColor:'#143652',//Pie chart colour
+	$('.chart').easyPieChart({
+		barColor: '#143652',//Pie chart colour
 		trackColor: '#e8e8e8',
 		scaleColor: false,
-		lineWidth : 5,
+		lineWidth: 5,
 		animate: 2000,
-		onStep: function(from, to, percent) {
+		onStep: function (from, to, percent) {
 			$(this.el).find('span').text(Math.round(percent));
 		}
-	});  
-	
+	});
+
 	// Progress Bar 
-	// made with jqueru ui
-	$(function() {
-		$('.progress-bar').each(function() {
-			var bar_value = $(this).attr('aria-valuenow') + '%';                
+	// made with jquery ui
+	$(function () {
+		$('.progress-bar').each(function () {
+			var bar_value = $(this).attr('aria-valuenow') + '%';
 			$(this).animate({ width: bar_value }, { duration: 1800, easing: 'linear' });
 		});
 	});
